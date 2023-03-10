7e57b05896a6f05f7ff2c5eaa4edb596c397ddde
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-10 07:40:32 +0100

    testing

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ let cron = require("node-cron");
 cron.schedule("* * * * * *", () => {
   console.log(`Running a task every minute`);
 });
-// * represents day of the week
-//* * represents month
-// * * * represents day of the month
-// * * * * represents hour
-// * * * * * represents minute
-// * * * * * * represents second (0-59)
+// * represents day of the week ; format : * or 0-7(where it starts from sunday and ends in sunday)
+//* * represents month ; format : * or 1-12 or jan feb..
+// * * * represents day of the month : * or 1-31
+// * * * * represents hour ; format : * or 0-23
+// * * * * * represents minute ; format: * or 0-59
+// * * * * * * represents second ; format : * or (0-59)
