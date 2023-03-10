bb86a813ef388ea7628582afa70c10c4e1abe069
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-10 07:49:32 +0100

    testing

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ cron.schedule("* * * * * *", () => {
 // * * * * represents hour ; format : * or 0-23
 // * * * * * represents minute ; format: * or 0-59
 // * * * * * * represents second ; format : * or (0-59)
+
+//making ranges
+
+cron.schedule("3-10 * * * * *", () => {
+  console.log(`Running a task every minute`);
+});
+
+//passing your schedule to a variable
+const task = cron.schedule("* * * * * *", () => {
+  console.log(`Running`);
+});
+
+task.start();
+//this is run the command and start the above function
+task.stop();
+//this is run the command and stop the above function
