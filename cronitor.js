1085b7f47169014c71864cc873f3450ccd3f7b27
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-13 16:07:53 +0100

    saved

--- cronitor.js
+++ cronitor.js
@@ -1,4 +1,5 @@
 const nodeCron = require("node-cron");
+const cronitor = require("cronitor")("cronitor-apiKey");
 
 nodeCron.schedule("*/5 * * * * *", function () {
   console.log(`sending welcome email to signed up users every five seconds`);
